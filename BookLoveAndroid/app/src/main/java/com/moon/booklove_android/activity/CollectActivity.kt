bc11c3d42e823ab02b6547db888d292db4bc2f7b
moonyy7012 <moonyy7012@naver.com>
2022-03-17 21:00:19 +0900

    안드로이드 유저 정보 수집 UI 진행중

--- BookLoveAndroid/app/src/main/java/com/moon/booklove_android/activity/CollectActivity.kt
+++ BookLoveAndroid/app/src/main/java/com/moon/booklove_android/activity/CollectActivity.kt
@@ -2,7 +2,10 @@ package com.moon.booklove_android.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.moon.booklove_android.R
 import com.moon.booklove_android.databinding.ActivityCollectBinding
+import com.moon.booklove_android.fragment.CollectGenderFragment
 
 class CollectActivity : AppCompatActivity() {
 
@@ -10,6 +13,10 @@ class CollectActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityCollectBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val transaction = supportFragmentManager.beginTransaction().replace(R.id.frameLayout, CollectGenderFragment())
+        transaction.commit()
+
     }
 
 
