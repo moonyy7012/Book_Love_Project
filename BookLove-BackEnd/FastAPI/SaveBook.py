1ecf3a76ec9227ffcd57480b3a32f3dcf7f6287e
박 명 우 <pmw7130@naver.com>
2022-03-21 10:12:52 +0900

    [Feat] 데이터 전처리

---
+++ BookLove-BackEnd/FastAPI/SaveBook.py
@@ -0,0 +1,35 @@
+import csv
+# ttbpmw71301115001
+import requests
+import json
+import pandas as pd
+
+def saveBook():
+    maxResult = 50
+    queryType = ["ItemNewAll", "ItemNewSpecial", "Bestseller"]
+    key = "ttbpmw71301115001"
+    dataList = ['title', 'link', 'categoryId', 'priceSales', 'isbn', 'author', 'categoryName']
+    dataLen = len(dataList)
+    pageNum = 20 #가져올 페이지
+    bookResult= []
+    #1페이지부터 원하는 페이지까지 가져오기
+    for page in range(1,pageNum+1):
+        url = f"http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbpmw71301115001&QueryType={queryType[2]}&MaxResults={maxResult}&start={page}&SearchTarget=Book&output=js&Version=20131101"
+        response = requests.get(url)
+        responseJson = json.loads(response.text) #response를 json으로 변환
+        itemList = responseJson
+        #책리스트 초기화
+        bookList = [[0] * dataLen for _ in range(maxResult)]
+        for i in range(maxResult):
+            for j in range(7):
+                bookList[i][j] = itemList['item'][i][dataList[j]]
+
+        if page ==1 :
+            bookResult+=bookList
+        else :
+            bookResult+=bookList
+    df = pd.DataFrame(bookResult,
+                      columns=['title', 'link', 'categoryId', 'priceSales', 'isbn', 'author', 'categoryName'])
+    df.to_csv('test.csv', index=False, encoding='cp949')
+
+saveBook()
