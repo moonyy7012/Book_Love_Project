704cac63135564f80219798575c70f24ca968488
박 명 우 <pmw7130@naver.com>
2022-04-03 03:26:20 +0900

    [Fix] 최근클릭 업데이트날짜로 수정

--- BookLove-BackEnd/FastAPI/main.py
+++ BookLove-BackEnd/FastAPI/main.py
@@ -8,5 +8,5 @@ app.include_router(data.router)
 app.include_router(recommend.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = "localhost", port=8000, reload=True)
-    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run("main:app", host = "localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
