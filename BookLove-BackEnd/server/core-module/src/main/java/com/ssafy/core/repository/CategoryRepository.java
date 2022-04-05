df56896f26e56a8beca39f4302f0959d22ce6c57
양아름 <yeveyn@gmail.com>
2022-04-05 11:18:16 +0900

    [Style] 코드 정리

--- BookLove-BackEnd/server/core-module/src/main/java/com/ssafy/core/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.core.repository;
-
-import com.ssafy.core.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>, CategoryRepoCommon {
-
-}
