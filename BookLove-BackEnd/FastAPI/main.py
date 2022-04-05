1278ef9cdedb2a807c2f778519dba189ab3e5ff3
양아름 <yeveyn@gmail.com>
2022-04-05 10:58:09 +0900

    [Style] 코드 정리

--- BookLove-BackEnd/FastAPI/main.py
+++ BookLove-BackEnd/FastAPI/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(recommend.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = "j6d106.p.ssafy.io", port=8000, reload=True)
+     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
